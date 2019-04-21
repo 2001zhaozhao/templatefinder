6e03a65714d12ebed3916cad77348a02f8b4e19b
Ian Balaguera <balagueraian@gmail.com>
2019-04-21 13:30:28 -0400

    Change output to command line

--- templatefinder/src/main/java/database_templatefinder/templatefinder/App.java
+++ templatefinder/src/main/java/database_templatefinder/templatefinder/App.java
@@ -250,20 +250,19 @@ public class App {
 		System.out.println("Program startup took " + (endTime - startTime) + "ms for " + templates.size() + " templates.");
 		
 		// Start web server
-		try {
+		/*try {
 			new HttpHandler();
 		} catch (IOException e) {
 			System.err.println("Couldn't start web server:\n" + e);
-		}
+		}*/
 		
 		// Interactive cmdline starts here
 		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
 		while(true) {
-			System.out.println("Please input the string to check.");
 			String line = scanner.nextLine();
-			
-			InputProcessing.legacyProcessInput(line, System.out);
+			JsonArray js = InputProcessing.toJson(InputProcessing.process(line));
+			System.out.print(js.toString());
 		}
 	}
 
